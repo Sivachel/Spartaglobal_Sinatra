4b383787629650a13eac19cf34944cebec024652
Sivachel <lsivachelvan@spartaglobal.com>
2018-08-24 17:12:05 +0100

    friday lecture done

--- Sinarta_intro/controllers/posts_controllers.rb
+++ Sinarta_intro/controllers/posts_controllers.rb
@@ -35,56 +35,57 @@ class PostsController < Sinatra::Base
 
   get "/" do
     @title = "Blog Posts"
-    @posts = $posts
+    @posts = Post.all
     erb :'posts/index'
   end
 
   get "/new" do
     @title = 'New'
 
-    @post = {
-      id: "",
-      title: "",
-      post_body:""
-    }
+    @post = Post.new
+
     erb :'posts/new'
   end
 
   get "/:id" do
     id = params[:id].to_i
-    @post = $posts[id]
+    @post = Post.find(id)
     erb :'posts/show'
   end
 
   post "/" do
-    new_post = {
-      id: $posts.length,
-      title: params[:title],
-      post_body: params[:post_body]
-    }
-    $posts.push(new_post)
+    post = Post.new
+
+    post.title = params[:title]
+    post.post_body = params[:post_body]
+
+    post.save
+
     redirect "/"
   end
 
   put "/:id" do
     id = params[:id].to_i
-    post = $posts[id]
-    post[:title] = params[:title]
-    post[:post_body] = params[:post_body]
-    $posts[id] = post
+
+    post = Post.find(id)
+
+    post.title = params[:title]
+    post.post_body = params[:post_body]
+    post.save
     redirect "/"
   end
 
   delete "/:id" do
     id = params[:id].to_i
-    $posts.delete_at(id)
+    Post.destroy(id)
+
     redirect "/"
   end
 
   get "/:id/edit" do
     @title = 'Edit'
     id = params[:id].to_i
-    @post = $posts[id]
+    @post = Post.find(id)
     erb :'posts/edit'
   end
 
